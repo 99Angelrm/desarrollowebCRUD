70ff42005e1dee80b1d108b93c2263727ae601f3
Diana <a01745778@itesm.mx>
2021-10-03 18:19:12 -0500

    CRUDEQ2

---
+++ index.js
@@ -0,0 +1,30 @@
+//Importando la biblioteca express
+const express = require('express')
+const path = require('path') // Windows \  Mac o Linux /
+//Cargar el objeto de conexiónn a la base
+const sequelize = require('./utils/database')
+
+//Importar rutas
+const vjRoutes = require('./routes/videojuegos')
+//Crear una aplicación web
+const app = express();
+
+//Establecer un middleware que configure donde se encuentran los
+//recursos públicos
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//Vinculamos la aplicación con los recursos de videojuegos
+app.use('/videojuegos',vjRoutes);
+
+//sequelize.sync({force:true})
+sequelize.sync()
+    .then(
+        app.listen(8081,()=>{
+            console.log("Servidor online en el puerto 8081")
+        })
+    )
+    .catch(err=>console.log(err))
+
+
---
+++ routes/videojuegos.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const vjController = require('../controllers/videojuegos')
+const router = express.Router()
+
+
+//Formulario de ingreso de datos (front-end) 
+//router.get('/agregarVideojuego',vjController.getAgregarVideojuego)
+//Servicio para el procesamiento de los datos
+router.post('/agregarVideojuego',vjController.postAgregarVideojuego)
+//Pagina de confirmacion (front-end)
+//router.get('/confirmacionDatos',vjController.getConfirmacionDatos)
+//Visualización de los datos guardados
+router.get('/obtenerVideojuegos',vjController.getObtenerVideojuegos)
+router.post('/borrarVideojuego',vjController.postBorrarVideojuego)
+router.post('/actualizarVideojuego',vjController.postActualizarVideojuego)
+
+
+module.exports = router
